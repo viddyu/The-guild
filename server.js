1c489d75b2fd031be9dbc05873d31d346bfff96c
Brandon Triplett <btriplett216757@gmail.com>
2018-05-14 14:08:46 -0400

    Created controllers folder and file, added syntax to server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(process.cwd() + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/guild_controller.js");
+
+app.use("/", routes);
+
+app.listen(port, function(){
+	console.log("Connected on port " + port);
+});
