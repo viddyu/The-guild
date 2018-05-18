0de69d905a5a132bfabe5fc60aa78802c915cf1c
Brandon Triplett <btriplett216757@gmail.com>
2018-05-18 15:19:28 -0400

    Chat box responsive

--- chat/assets/js/chatapp.js
+++ chat/assets/js/chatapp.js
@@ -1,3 +1,22 @@
 /*var chat = require('chat'),
     room = chat.room();
     client.write('hello');*/
+
+//Initializing socket, connection to server
+var socket = io.connect('http://localhost:3000');
+socket.on('connect', function(data) {
+    socket.emit('join', 'Hello server from client');
+});
+
+//Listener for 'thread' event, which updates messages
+socket.on('thread', function(data) {
+    $('#thread').append('<li>' + data + '</li>');
+});
+
+//Prevents form from submitting and sends a message to server
+$('form').submit(function() {
+    var message = $('#message').val();
+    socket.emit('messages', message);
+    this.reset();
+    return false;
+});
